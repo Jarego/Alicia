436a0a8003eb03589a9b52020cb3f97cd02e02dd
Jarego <jaregocreepy@hotmail.com>
2019-04-27 23:01:03 -0500

    Add files via upload

---
+++ WindowsApp4/WindowsApp4/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(473, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(473, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(470, 98)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 16)
+        Me.Label1.TabIndex = 2
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "basico.png")
+        Me.ImageList1.Images.SetKeyName(1, "q0.png")
+        Me.ImageList1.Images.SetKeyName(2, "q1.png")
+        Me.ImageList1.Images.SetKeyName(3, "q2.png")
+        Me.ImageList1.Images.SetKeyName(4, "q3.png")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp4.My.Resources.Resources._0
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(444, 258)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.White
+        Me.PictureBox2.Location = New System.Drawing.Point(82, 108)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(32, 30)
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.White
+        Me.PictureBox3.Location = New System.Drawing.Point(231, 108)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(32, 30)
+        Me.PictureBox3.TabIndex = 7
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.White
+        Me.PictureBox4.Location = New System.Drawing.Point(380, 108)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(32, 30)
+        Me.PictureBox4.TabIndex = 8
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.White
+        Me.PictureBox5.Location = New System.Drawing.Point(303, 228)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(28, 26)
+        Me.PictureBox5.TabIndex = 9
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 275)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+End Class
---
+++ WindowsApp4/WindowsApp4/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+    Dim res As String
+    Dim cont = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text + " "
+        S1()
+
+    End Sub
+    Sub S1()
+        If TextBox1.Text(cont) = "1" Then
+            res += "1"
+            cont += 1
+            Label1.Text = res
+            PictureBox2.Visible = False
+            PictureBox3.Visible = True
+            PictureBox4.Visible = True
+            PictureBox5.Visible = True
+            MsgBox("Encuentra 1, se mantiene igual, se mueve a la dercha")
+            S1()
+
+        ElseIf TextBox1.Text(cont) = "0" Then
+            res += "0"
+            cont += 1
+            Label1.Text = res
+            PictureBox2.Visible = False
+            PictureBox3.Visible = True
+            PictureBox4.Visible = True
+            PictureBox5.Visible = True
+            MsgBox("Encuentra 0, se mantiene igual, se mueve a la dercha")
+
+            S1()
+        ElseIf TextBox1.Text(cont) = " " Then
+            res += " "
+            cont -= 1
+            Label1.Text = res
+            PictureBox2.Visible = True
+            PictureBox3.Visible = False
+            PictureBox4.Visible = True
+            PictureBox5.Visible = True
+            MsgBox("Encuentra b, se mantiene igual, se mueve a la izquierda")
+            S2()
+        End If
+    End Sub
+    Sub S2()
+        If TextBox1.Text(cont) = "1" Then
+            res = res.Remove(cont, 1).Insert(cont, "0")
+            cont -= 1
+            Label1.Text = res
+            PictureBox2.Visible = True
+            PictureBox3.Visible = False
+            PictureBox4.Visible = True
+            PictureBox5.Visible = True
+            MsgBox("Encuentra 1, se cambia a 0, se mueve a la izquierda")
+            S2()
+        ElseIf TextBox1.Text(cont) = "0" Then
+            res = res.Remove(cont, 1).Insert(cont, "1")
+            cont -= 1
+            Label1.Text = res
+            PictureBox2.Visible = True
+            PictureBox3.Visible = True
+            PictureBox4.Visible = False
+            PictureBox5.Visible = True
+            MsgBox("Encuentra 0, se cambia a 1, se mueve a la izquierda")
+            S3()
+        ElseIf TextBox1.Text(cont) = " " Then
+            res = res.Remove(cont, 1).Insert(cont, "1")
+            PictureBox1.Image = ImageList1.Images(1)
+            Label1.Text = res
+            cont = 0
+            PictureBox2.Visible = True
+            PictureBox3.Visible = True
+            PictureBox4.Visible = True
+            PictureBox5.Visible = False
+            MsgBox("Estado final")
+            PictureBox2.Visible = True
+            PictureBox3.Visible = True
+            PictureBox4.Visible = True
+            PictureBox5.Visible = True
+        End If
+    End Sub
+
+    Sub S3()
+        If cont >= 0 Then
+            If TextBox1.Text(cont) = "0" Then
+                res = res.Remove(cont, 1).Insert(cont, "0")
+
+                cont -= 1
+                Label1.Text = res
+                PictureBox2.Visible = True
+                PictureBox3.Visible = True
+                PictureBox4.Visible = False
+                PictureBox5.Visible = True
+                MsgBox("Encuentra 0, se mantiene igual, se mueve a la izquierda")
+                S3()
+            ElseIf TextBox1.Text(cont) = "1" Then
+                res = res.Remove(cont, 1).Insert(cont, "1")
+                cont -= 1
+                Label1.Text = res
+                Label1.Text = res
+                PictureBox2.Visible = True
+                PictureBox3.Visible = True
+                PictureBox4.Visible = False
+                PictureBox5.Visible = True
+                MsgBox("Encuentra 1, se mantiene igual, se mueve a la izquierda")
+                S3()
+            ElseIf TextBox1.Text(cont) = "" Then
+                Label1.Text = res
+                cont = 0
+                Label1.Text = res
+                PictureBox2.Visible = True
+                PictureBox3.Visible = True
+                PictureBox4.Visible = True
+                PictureBox5.Visible = False
+                MsgBox("Estado final")
+            End If
+        Else
+            Label1.Text = res
+            cont = 0
+            PictureBox2.Visible = True
+            PictureBox3.Visible = True
+            PictureBox4.Visible = True
+            PictureBox5.Visible = False
+            MsgBox("Estado final")
+        End If
+    End Sub
+
+End Class
